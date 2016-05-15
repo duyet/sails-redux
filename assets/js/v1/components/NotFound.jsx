35fa279863401efb51f4da24e7cdb921e7d21e49
Van-Duyet Le <lvduit08@gmail.com>
2016-05-15 15:41:47 +0700

    NotFound page

--- assets/js/v1/components/NotFound.jsx
+++ assets/js/v1/components/NotFound.jsx
@@ -1,9 +1,15 @@
-define(['react', '../containers/Header', '../containers/AddTodo', '../containers/VisibleTodoList', './Footer'],
-function (React, Header, AddTodo, VisibleTodoList, Footer) {
+define(['react'],
+function (React) {
   const App = () => (
-    <div className='not-found'>
-      <h1>404</h1>
-    </div>
+
+	<div className='container-fluid not-found' style={{ margin: '100px 0' }}>
+  	<div className='row'>
+		<div className='col-md-12 text-center'>
+			<img src='/images/browser.svg' className='img-responsive' style={{ width: 250, display: 'inline-block' }} />
+			<h1>Page Under Construction</h1>
+		</div>
+	</div>
+	</div>
   )
 
   return App
